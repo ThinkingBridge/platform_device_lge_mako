2e322ff8b84883ea199649ed884ccffe243b670b
htchoi1995 <htchoi1995@gmail.com>
2014-01-01 11:44:46 +0900

    mako: cleanup for thinkingbridge

---
+++ thinkingbridge.mk
@@ -0,0 +1,21 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 768
+
+# Inherit some common ThinkingBridge stuff.
+$(call inherit-product, vendor/thinkingbridge/config/common.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lge/mako/full_mako.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := mako
+PRODUCT_NAME := thinkingbridge_mako
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 4
+PRODUCT_MANUFACTURER := LGE
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=hammerhead BUILD_FINGERPRINT=google/hammerhead/hammerhead:4.4.2/KOT49H/937116:user/release-keys PRIVATE_BUILD_DESC="hammerhead-user 4.4.2 KOT49H 937116 release-keys"
+
+# Enable Torch
+PRODUCT_PACKAGES += Torch
